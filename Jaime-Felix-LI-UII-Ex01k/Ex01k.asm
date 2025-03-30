a6b679aa0a262f766f800c6d95d58fbf53e7f493
Jaime-Felix <88902414+Jaime-Felix@users.noreply.github.com>
2025-03-30 16:55:54 -0700

    Update Ex01k.asm

--- Jaime-Felix-LI-UII-Ex01k/Ex01k.asm
+++ Jaime-Felix-LI-UII-Ex01k/Ex01k.asm
@@ -11,7 +11,7 @@ WriteConsoleW	PROTO	:QWORD,	:QWORD, :QWORD, :QWORD, :QWORD
 ExitProcess		PROTO	CodigoSalida:QWORD
 
 				.DATA
-; Definir las variables E, X, Y y Z, así como los mensajes de entrada correspondientes y el mensaje de salida
+; Definir las variables E, X, Y y Z, asÃ­ como los mensajes de entrada correspondientes y el mensaje de salida
 E				QWORD	0
 
 MenEnt01		WORD	'P', 'r', 'o', 'p', 'o', 'r', 'c', 'i', 'o', 'n', 'e', ' ', 'e', 'l', ' ', 'v', 'a', 'l', 'o', 'r', ' ', 'd', 'e', ' ', 'X', ':', ' '
@@ -21,77 +21,73 @@ MenSal01		WORD	'E', 'l', ' ', 'v', 'a', 'l', 'o', 'r', ' ', 'd', 'e', ' ', 'E', 
 ManejadorE		QWORD	?
 ManejadorS		QWORD	?
 Caracteres		QWORD	?
-Texto			WORD	13 DUP ( ? )				; Variable temporal para conversión Entero - Cadena, Cadena - Entero
+Texto			WORD	13 DUP ( ? )				; Variable temporal para conversiÃ³n Entero - Cadena, Cadena - Entero
 SaltoLinea		WORD	13, 10
-STD_INPUT		EQU		-10
-STD_OUTPUT		EQU		-11
+STD_INPUT		EQU	-10
+STD_OUTPUT		EQU	-11
 
 				.CODE
-Principal		PROC
+Principal			PROC
 
 				; Alinear espacio en la pila
 				SUB		RSP, 40
 
-				; Obtener manejador estándar del teclado
+				; Obtener manejador estÃ¡ndar del teclado
 				MOV		RCX, STD_INPUT
-				CALL	GetStdHandle
+				CALL		GetStdHandle
 				MOV		ManejadorE, RAX
 
-				; Obtener manejador estándar de la consola
+				; Obtener manejador estÃ¡ndar de la consola
 				MOV		RCX, STD_OUTPUT
-				CALL	GetStdHandle
+				CALL		GetStdHandle
 				MOV		ManejadorS, RAX
 
 				; Solicitar la variable X
 				MOV		RCX, ManejadorS				; Manejador de la consola donde se escribe
-				LEA		RDX, MenEnt01				; Dirección de la cadena a escribir
-				MOV		R8, LENGTHOF MenEnt01		; Número de caracteres a escribir
-				LEA		R9, Caracteres				; Dirección de la variable donde se guarda el total de caracteres escrito
-				MOV		R10, 0						; Reservado para uso futuro
-				CALL	WriteConsoleW
+				LEA		RDX, MenEnt01				; DirecciÃ³n de la cadena a escribir
+				MOV		R8, LENGTHOF MenEnt01			; NÃºmero de caracteres a escribir
+				LEA		R9, Caracteres				; DirecciÃ³n de la variable donde se guarda el total de caracteres escrito
+				CALL		WriteConsoleW
 
 				MOV		RCX, ManejadorE				; Manejador del teclado donde se lee la cadena
-				LEA		RDX, Texto					; Dirección de la cadena a leer
-				MOV		R8, LENGTHOF Texto			; Número de caracteres máximo a leer
-				LEA		R9, Caracteres				; Dirección de la variable donde se guarda el total de caracteres leídos
-				MOV		R10, 0						; Reservado para uso futuro
-				CALL	ReadConsoleW
+				LEA		RDX, Texto				; DirecciÃ³n de la cadena a leer
+				MOV		R8, LENGTHOF Texto			; NÃºmero de caracteres mÃ¡ximo a leer
+				LEA		R9, Caracteres				; DirecciÃ³n de la variable donde se guarda el total de caracteres leÃ­dos
+				CALL		ReadConsoleW
 				MacroCadenaAEntero	Texto, X
 
 				; Solicitar la variable Y
 				; Solicitar la variable Z
 
-				; Evaluar la expresión E dependiendo del valor de X
+				; Evaluar la expresiÃ³n E dependiendo del valor de X
 				;SI X < 0 ENTONCES
-   				;	E = X * Y * Z
+Â  Â 				;	E = X * Y * Z
 				;SINO
-   				;	E = X + Y + Z
+Â  Â 				;	E = X + Y + Z
 				;FINSI
 
 				; Mostrar el mensaje de salida
 				MOV		RCX, ManejadorS				; Manejador de la consola donde se escribe
-				LEA		RDX, MenSal01				; Dirección de la cadena a escribir
-				MOV		R8, LENGTHOF MenSal01		; Número de caracteres a escribir
-				LEA		R9, Caracteres				; Dirección de la variable donde se guarda el total de caracteres escrito
-				MOV		R10, 0						; Reservado para uso futuro
-				CALL	WriteConsoleW
+				LEA		RDX, MenSal01				; DirecciÃ³n de la cadena a escribir
+				MOV		R8, LENGTHOF MenSal01			; NÃºmero de caracteres a escribir
+				LEA		R9, Caracteres				; DirecciÃ³n de la variable donde se guarda el total de caracteres escrito
+				CALL		WriteConsoleW
 
 				; Convertir E a cadena
 				MacroEnteroACadena	E, Texto, Caracteres
 
 				; Mostrar el valor de E en forma de cadena
 
-				; Salto de línea
+				; Salto de lÃ­nea
 				MOV		RCX, ManejadorS				; Manejador de la consola donde se escribe
-				LEA		RDX, SaltoLinea				; Dirección de la cadena a escribir
-				MOV		R8, LENGTHOF SaltoLinea		; Número de caracteres a escribir
-				LEA		R9, Caracteres				; Dirección de la variable donde se guarda el total de caracteres escrito
-				MOV		R10, 0						; Reservado para uso futuro
-				CALL	WriteConsoleW
+				LEA		RDX, SaltoLinea				; DirecciÃ³n de la cadena a escribir
+				MOV		R8, LENGTHOF SaltoLinea			; NÃºmero de caracteres a escribir
+				LEA		R9, Caracteres				; DirecciÃ³n de la variable donde se guarda el total de caracteres escrito
+				CALL		WriteConsoleW
 
 				; Salir al sistema operativo
 				MOV		RCX, 0
-				CALL	ExitProcess
+				CALL		ExitProcess
 
-Principal		ENDP
+Principal			ENDP
 				END
